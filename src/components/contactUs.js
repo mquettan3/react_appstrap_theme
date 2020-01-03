b396cd3316b5a2451fddeefb058ec8a3303c0aaa
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2020-01-03 13:31:46 -0500

    Updating contact section to look good on mobile.  Everything is done now except for the links.  None of the links work.

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -12,7 +12,7 @@ export default function contactUs() {
                 <div className="info">
                     <h3>Contact Us</h3>
                     <p><Icon size={24} icon={phone}></Icon>703-335-0654</p>
-                    <p><Icon size={24} icon={envelope}></Icon>someone@bristowelectronics.com</p>
+                    <p><Icon size={24} icon={envelope}></Icon>someone@kanddelectric.com</p>
                     <p><Icon size={24} icon={mapMarker}></Icon>Bristow, VA</p>
                     <div className="section-divider"></div>
                     <p>When was the last time that your Bristow, VA, electrician actually showed up on time and did the job properly the first time? If you need some electrical work completed in a timely manner, then give us a call today! K & D Electric Company LLC is the only name you need to know in the electrical business.</p>
